92f61b56970c8cfdc3aa107071228460182baad1
NvidiaFR3 <pasukankrgtidur@gmail.com>
2025-07-10 17:18:38 +0700

    Update brat.js

--- api/brat.js
+++ api/brat.js
@@ -1,11 +1,10 @@
 module.exports = {
-  name: "Bratvid",
-  desc: "Brat video generator",
+  name: "Brat",
+  desc: "Brat Image generator",
   category: "Imagecreator",
-  path: "/imagecreator/bratvid?text=",
+  path: "/imagecreator/brat?text=",
   async run(req, res) {
-    const { apikey, text } = req.query;
-    if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' });
+    const { text } = req.query;
     if (!text) return res.json({ status: false, error: 'Missing text' });
 
     const buffer = await getBuffer(`https://fastrestapis.fasturl.cloud/maker/brat/animated?text=${encodeURIComponent(text)}&mode=animated`);
