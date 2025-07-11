94df76cc49e0907737ab0a414a15f21cca2d7575
NvidiaFR3 <pasukankrgtidur@gmail.com>
2025-07-11 23:32:53 +0700

    Update addadmin.js

--- api/addadmin.js
+++ api/addadmin.js
@@ -20,7 +20,7 @@ module.exports = {
         },
         body: JSON.stringify({
           username,
-          email: `${username}@fr3panel.local`,
+          email: `${username}@fr3.com`,
           first_name: "admin",
           last_name: username,
           password: username,
