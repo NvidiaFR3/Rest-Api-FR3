14d00adfe338ec4b02708445dd423cfa664accc6
NvidiaFR3 <pasukankrgtidur@gmail.com>
2025-07-13 01:31:01 +0700

    Update text-qr.js

--- api/text-qr.js
+++ api/text-qr.js
@@ -2,7 +2,7 @@ const qrcode = require('qrcode');
 
 module.exports = {
   name: "Text to QR",
-  desc: "Mengubah teks menjadi QR Code",
+  desc: "Generate QR code dari teks dan langsung kirim sebagai image/png",
   category: "Tools",
   path: "/tools/texttoqr?text=",
   async run(req, res) {
@@ -16,17 +16,13 @@ module.exports = {
     }
 
     try {
-      const qr = await qrcode.toDataURL(text, {
-        type: 'image/png',
-        errorCorrectionLevel: 'H',
+      const buffer = await qrcode.toBuffer(text, {
+        type: 'png',
+        errorCorrectionLevel: 'H'
       });
 
-      res.status(200).json({
-        creator: "FR3HOSTING",
-        status: true,
-        text,
-        qr_base64: qr
-      });
+      res.setHeader('Content-Type', 'image/png');
+      res.send(buffer);
     } catch (err) {
       res.status(500).json({
         creator: "FR3HOSTING",
