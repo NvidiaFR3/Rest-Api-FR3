069a444292766b1336b116cc22df409e3c185dfa
NvidiaFR3 <pasukankrgtidur@gmail.com>
2025-09-07 06:33:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.set("json spaces", 2);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/', express.static(path.join(__dirname, '/')));
-app.use('/', express.static(path.join(__dirname, 'ui')));
 global.getBuffer = async (url, options = {}) => {
   try {
     const res = await axios({
