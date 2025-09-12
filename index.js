7533c2cd29afa0702d5d0956b5f59885bb3ff6aa
NvidiaFR3 <pasukankrgtidur@gmail.com>
2025-09-12 22:17:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/downloads", express.static(path.join(__dirname)));
-
 global.getBuffer = async (url, options = {}) => {
   try {
     const res = await axios({
